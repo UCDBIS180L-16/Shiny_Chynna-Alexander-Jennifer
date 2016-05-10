da685499a9bef20637f16d2a4a977427c43d8247
C. Gabotero <clgabotero@ucdavis.edu>
2016-05-10 13:34:23 -0700

    Added dropdown on ui

--- RiceSNP_ACJ/server.R
+++ RiceSNP_ACJ/server.R
@@ -9,7 +9,6 @@ data.geno.2500 <- data.geno[ , sample(ncol(data.geno), 2500)]
 geno.numeric <- data.matrix(data.geno.2500)
 genDist <- as.matrix(dist(geno.numeric))
 geno.mds <- as.data.frame(cmdscale(genDist))
-data.merged <- merge(geno.mds,data.pheno, by.x = "row.names", by.y = 1)
 data.geno.pheno <- merge(geno.mds, data.pheno, by= "row.names")
 data.geno.2500.c <- apply(data.geno.2500, 2, as.character)
 data.geno.2500.ps <- matrix("", nrow=nrow(data.geno.2500.c)* 2, ncol = ncol(data.geno.2500.c))
@@ -18,7 +17,7 @@ for (i in 1:nrow(data.geno.2500.c)) {
   data.geno.2500.ps[(i - 1) * 2 + 2,] <- substr(data.geno.2500.c[i, ], 2, 2)
 }
 load("~/ps4.2500.RData")
-
+names(ps4)
 round(head(ps4$AmPr), 3)
 table(ps4$AmId)
 ps4.df <- as.data.frame(cbind(round(ps4$AmPr, 3), ps4$AmId))
@@ -30,8 +29,9 @@ ps4.df.melt <- melt(ps4.df,id.vars=c("popID","sampleID"))
 geno.mds$popID <- factor(ps4$AmId)
 colnames(ps4$AmPr) <- paste("pr", 1:4, sep = "")
 geno.mds <- cbind(geno.mds, ps4$AmPr)
-data.pheno.mds <- merge(geno.mds,data.pheno,by.x = "row.names", by.y = 1,all=T)
-
+save(data.pheno, geno.mds, file = "data_from_SNP_lab.Rdata")
+load("data_from_SNP_lab.Rdata")
+data.pheno.mds <- merge(geno.mds, data.pheno, by = "row.names", all=T)
 
 # Define server logic required to draw a boxplot
 shinyServer(function(input, output) {
@@ -46,16 +46,12 @@ shinyServer(function(input, output) {
   output$boxPlot <- renderPlot({
     
     # set up the plot
-    data.pheno.mds.melt <- melt(data.pheno.mds,id.var="Region")
-    
-    
-    
-     pl <- ggplot(data = data.pheno.mds.melt.subs,
+    pl <- ggplot(data = data.pheno.mds,
                  #Use aes_string below so that input$trait is interpreted
                  #correctly.  The other variables need to be quoted
-                 aes_string(x="Region",
-                            y="trait",
-                            fill="Region"
+                 aes_string(x=input$popID,
+                            y=input$trait,
+                            fill="popID"
                  )
     )
     
@@ -63,5 +59,3 @@ shinyServer(function(input, output) {
     pl + geom_boxplot()
   })
 })
-
-
--- RiceSNP_ACJ/ui.R
+++ RiceSNP_ACJ/ui.R
@@ -4,23 +4,26 @@ library(shiny)
 shinyUI(fluidPage( #create the overall page
   
   # Application title
-  titlePanel("Iris Data"),
+  titlePanel("Rice SNPs by Ancestral Populations"),
   
   # Some helpful information
-  helpText("This application creates a boxplot to show difference between",
-           "iris species.  Please use the radio box below to choose a trait",
-           "for plotting"),
+  helpText("This application allows the user to create a boxplot, historgram, and violin plot
+           of Rice SNP data from several ancestral rice populations for several traits."),
   
   # Sidebar with a radio box to input which trait will be plotted
   sidebarLayout(
     sidebarPanel(
+      selectInput("popID", "Population:",
+                  list("Population 1" = "1", 
+                       "Population 2" = "2",
+                       "Population 3" = "3",
+                       "Population 4" = "4")),
       radioButtons("trait", #the input variable that the value will go into
                    "Choose a trait to display:",
                    c("Alu.Tol",
-                     "Plant.height",
-                     "Panicle.length",
-                     "Seed.length",
-                     "Seed.width")
+                     "Amylose.content",
+                     "Alkali.spreading.value",
+                     "Protein.content")
       )),
     
     # Show a plot of the generated distribution

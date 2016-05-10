9a1e6090c9f5d68a812a725f19838aff0e065de9
C. Gabotero <clgabotero@ucdavis.edu>
2016-05-10 15:21:04 -0700

    Got stupid thing to work

--- RiceSNP_ACJ/ui.R
+++ RiceSNP_ACJ/ui.R
@@ -4,38 +4,25 @@ library(shiny)
 shinyUI(fluidPage( #create the overall page
   
   # Application title
-  titlePanel("RICE SNP Population Dynamics"), 
+  titlePanel("Iris Data"),
   
   # Some helpful information
-  helpText("This application creates a scatterplot to display SNPs and",
-           "associations with traits in rice. Please use the radio box",
-          "below to choose a trait",
+  helpText("This application creates a boxplot to show difference between",
+           "iris species.  Please use the radio box below to choose a trait",
            "for plotting"),
   
   # Sidebar with a radio box to input which trait will be plotted
   sidebarLayout(
     sidebarPanel(
-      radioButtons("Plot",
-                   "Choose a plot to display:",
-                   c("Violin Plot",
-                     "Boxplot")),
-      
-      
-      radioButtons("trait", #the input variable that the value will go into
-                   "Choose a trait to display:",
-                   c("Alu.Tol",
-                     "Plant.height",
-                     "Panicle.length",
-                     "Seed.length",
-                     "Seed.width")
+      selectInput("trait", #the input variable that the value will go into
+                  label = "Choose a trait to display:",
+                  choices = c("Amylose.content",
+                              "Alu.Tol",
+                              "Protein.content")
       )),
-    radioButtons("popID",
-                 "Choose popID",
-                 c("1", "2", "3", "4")),
     
     # Show a plot of the generated distribution
-    mainPanel(plotOutput(input$plot))
+    mainPanel(plotOutput("boxPlot")
     )
   )
 ))
-

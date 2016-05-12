7469482d0816df04c7e5b259211223df4159adaa
jenkoo <jenkoo@ucdavis.edu>
2016-05-11 19:15:20 -0700

    fixed layout

--- RiceSNP_ACJ/ui.R
+++ RiceSNP_ACJ/ui.R
@@ -7,7 +7,7 @@ shinyUI(fluidPage( #create the overall page
   titlePanel("Rice SNP Data by Population"),
   
   # Some helpful information
-  helpText("This application creates boxplots to distinguish certain traits for",
+  helpText("This application creates boxplots and violin plots to distinguish certain traits for",
            "rice populations.  Please use the dropdown menu to choose a trait",
            "for plotting"),
   
@@ -19,19 +19,19 @@ shinyUI(fluidPage( #create the overall page
                   choices =  c("Violin",
                                "Boxplot")
                   
+      ),
+      
+      selectInput("trait",  #the input variable that the value will go into
+                  label = "Choose a trait to display:",
+                  choices = c("Amylose.content",
+                              "Alu.Tol",
+                              "Protein.content",
+                              "Panicle.number.per.plant",
+                              "Seed.number.per.panicle")
       )),
     
-    selectInput("trait", #the input variable that the value will go into
-                label = "Choose a trait to display:",
-                choices = c("Amylose.content",
-                            "Alu.Tol",
-                            "Protein.content",
-                            "Panicle.number.per.plant",
-                            "Seed.number.per.panicle")
-    )),
-  
-  # Show a plot of the generated distribution
-  mainPanel(plotOutput("plot")
+    # Show a plot of the generated distribution
+    mainPanel(plotOutput("plot")
+    )
   )
-)
-)
+))
